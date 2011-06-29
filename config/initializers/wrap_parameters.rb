b4a4b6854bf9c932180c2925b8fcd005c94e01f6
Ivan Dzyzenko <intale.a@gmail.com>
2011-06-29 20:22:40 +0300

    fixed strange issue #2

--- config/initializers/wrap_parameters.rb
+++ config/initializers/wrap_parameters.rb
@@ -1,4 +1,4 @@
-ActionController::Base.wrap_parameters format: [:json]
+ActionController::Base.wrap_parameters :format => [:json]
 
 if defined?(ActiveRecord)
   ActiveRecord::Base.include_root_in_json = false
