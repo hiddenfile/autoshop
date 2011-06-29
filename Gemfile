be079fe1416666cc271c4be8bb1e907caf9f6a41
Jenua Boiko <jeyboy@bigmir.net>
2011-06-29 11:23:30 +0300

    Finish refactoring of controller Products

--- Gemfile
+++ Gemfile
@@ -2,16 +2,11 @@ source 'http://rubygems.org'
 
 gem 'rails', '3.1.0.rc4'
 
-# Bundle edge Rails instead:
-# gem 'rails',     :git => 'git://github.com/rails/rails.git'
-
 gem 'therubyracer', '>= 0.8.2'
 
-#gem 'sqlite3'
 gem "mysql2", "0.3.2"
 gem 'devise'
 
-# Asset template engines
 gem 'sass-rails', "~> 3.1.0.rc"
 gem 'coffee-script'
 gem 'uglifier'
@@ -21,18 +16,7 @@ gem 'web-app-theme', :git => 'git://github.com/cncardoso/web-app-theme.git'
 gem 'haml'
 gem 'hpricot'
 
-gem 'yaml_db', :git => 'git://github.com/ludicast/yaml_db.git'
-
-# Use unicorn as the web server
-# gem 'unicorn'
-
-# Deploy with Capistrano
-# gem 'capistrano'
-
-# To use debugger
-# gem 'ruby-debug19', :require => 'ruby-debug'
 
 group :test do
-  # Pretty printed test output
   gem 'turn', :require => false
 end
--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -1,26 +1,12 @@
 class Admin::ProductsController < Admin::AdminController
   def create
-
-#    user=User.find_by_name(params[:post][:user][:user])
-#
-#    @post=user.posts.build
-#    %w(title body).each do |attr|
-#      @post.send("#{attr}=",params[:post][attr])
-#    end
-
-#    params[:product][:company]=Company.find_by_name(params[:product][:company]).id
-#    params[:product][:group]=Group.find_by_name(params[:product][:group]).id
-
-#    params[:product][:company]=Company.find(params[:product][:company])
-#    params[:product][:group]=Group.find(params[:product][:group])
-
     @product = Product.new(params[:product])
 
-      if @product.save
-        redirect_to(admin_products_path, :notice => 'Product was successfully created.')
-      else
-        render :action => "new"
-      end
+    if @product.save
+      redirect_to(admin_products_path, :notice => 'Product was successfully created.')
+    else
+      render :action => "new"
+    end
   end
 
   def index
@@ -34,16 +20,13 @@ class Admin::ProductsController < Admin::AdminController
   end
 
   def update
-    params[:product][:company]=Company.find(params[:product][:company])
-    params[:product][:group]=Group.find(params[:product][:group])
-
     @product = Product.find(params[:id])
 
-      if @product.update_attributes(params[:product])
-        redirect_to(admin_product_path(@product), :notice => 'Product was successfully updated.')
-      else
-        render :action => "edit"
-      end
+    if @product.update_attributes(params[:product])
+      redirect_to(admin_product_path(@product), :notice => 'Product was successfully updated.')
+    else
+      render :action => "edit"
+    end
   end
 
   def show
@@ -56,5 +39,4 @@ class Admin::ProductsController < Admin::AdminController
     @companies = Company.all
     @groups = Group.all
   end
-
 end
