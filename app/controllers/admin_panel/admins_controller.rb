f202d2c9ca9caa262f099f3da70792eb0c911280
Boris <boris@Boris.(none)>
2011-07-07 17:37:35 +0300

    deleted sign_up from admin_panel and added possibility to add admins

--- app/controllers/admin_panel/admins_controller.rb
+++ app/controllers/admin_panel/admins_controller.rb
@@ -4,6 +4,21 @@ class AdminPanel::AdminsController < AdminPanel::AdminApplicationController
     @admins = Admin.all
   end
 
+  def new
+    @admin = Admin.new
+  end
+
+  def create
+       @admin = Admin.new(params[:admin])
+       if @admin.save
+           flash[:notice] = "Successful created"
+             redirect_to admin_panel_admins_path
+       else
+         flash[:alert] = "Error. Can not save."
+         render :action => :new
+       end
+  end
+
   def destroy
     if Admin.find(params[:id]).destroy
       redirect_to(admin_panel_admins_path, :notice => 'Admin was successfully deleted.')
--- app/models/admin.rb
+++ app/models/admin.rb
@@ -1,7 +1,16 @@
 class Admin < ActiveRecord::Base
 
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable
+  devise :database_authenticatable, :rememberable, :trackable
 
   attr_accessible :email, :password, :password_confirmation, :remember_me
+
+  validates :password, :presence => true, :confirmation => true, :length => {:minimum => 6}, :if => :password_require?
+
+  protected
+  def password_require?
+    new_record? || !password.nil? || !password_confirmation.nil?
+  end
+
+
+
 end
