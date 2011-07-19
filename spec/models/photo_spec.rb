043e8572d53381cc2e8e368c8bafc6c20fa1421c
Pilgun Boris <boris.pilgun@faceit.com.ua>
2011-07-19 17:30:41 +0300

    added tests for photo (not all)

--- spec/models/photo_spec.rb
+++ spec/models/photo_spec.rb
@@ -7,11 +7,11 @@ describe Photo do
     g.options[:polymorphic].should be_true
   end
 
-#  it "should have has_attached_file with params" do
-#   # @photo = Photo.new(:photo => File.new(Rails.root + 'spec/fixtures/images/rails.png'))
-#    #lambda{@photo.save!}.should_not
-#
-#    #should has_attached_file(:photo)
-#
-#  end
+  it "should have correctly save of test picture" do
+   @photo = Factory(:photo)
+   @photo.should_not be_nil
+
+    #need to test saving files
+  end
 end
+
