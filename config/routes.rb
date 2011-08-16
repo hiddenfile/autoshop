4804966031e2f20dac444f8ec902f1ac44b8f43e
sedzen <sedzen@ukr.net>
2011-08-16 12:35:26 +0300

    fix routes.
    closes #41

--- config/routes.rb
+++ config/routes.rb
@@ -8,13 +8,20 @@ Autoshop::Application.routes.draw do
   resources :searches, :except => [:new, :update, :create, :destroy, :index, :show, :edit] do
     post 'search_by_products', :on => :member
   end
-  resources :products, :except => [:new, :update, :create, :destroy]
-  resources :orders, :only => [:show,:index,:create, :destroy]
+
   resources :shop_carts,:except => [:new, :update, :create, :index, :edit] do
     get 'add_to_cart', :on => :member
     get 'remove_from_cart', :on => :member
   end
+
   resources :groups, :only => :show
+  resources :orders, :only => [:show,:index,:create, :destroy]
+
+  resources :companies, :only => :show do
+    resources :groups, :only => :show do
+      resources :products, :only => [:show,:index]
+    end
+  end
 
   match '/admin_panel' => 'admin_panel/users#index', :as => 'admin_users'
 
@@ -22,11 +29,5 @@ Autoshop::Application.routes.draw do
     resources :products, :companies , :groups, :users ,:admins
     resources :photos, :only => [:index, :destroy]
     resources :orders, :only => [:index,:show,:update,:edit]
-  end
-
-  resources :companies, :only => :show do
-    resources :groups, :only => :show do
-      resources :products, :only => [:show,:index]
-    end
   end
 end
