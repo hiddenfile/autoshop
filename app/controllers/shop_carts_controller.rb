fb084b0eab49a0d4b456e0a466cb916c98fb3d84
Jenua Boiko <jeyboy@bigmir.net>
2011-09-02 15:49:47 +0300

    small fix

--- app/controllers/shop_carts_controller.rb
+++ app/controllers/shop_carts_controller.rb
@@ -3,9 +3,7 @@ class ShopCartsController < ApplicationController
   def update
     if CartMethods.cookies_valid?(cookies)
 
-      if params[:shop_cart_items]
-        CartMethods.save_items_list(cookies,params[:shop_cart_items])
-      elsif params[:id] and params[:delete_item]
+      if params[:id] and params[:delete_item]
         CartMethods.remove_item_from_list(cookies,params[:id])
       elsif params[:id]
         CartMethods.add_item_to_list(cookies,params[:id],{:title=>params[:title],:price=>params[:price].to_f, :count => 1})
