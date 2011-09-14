938bad5b0a56aeef6d2f15c5c267e8e3a6999e4e
Jenua Boiko <jeyboy@bigmir.net>
2011-09-14 16:58:09 +0300

    Add fixes for cart and order

--- app/controllers/shop_carts_controller.rb
+++ app/controllers/shop_carts_controller.rb
@@ -3,13 +3,12 @@ class ShopCartsController < ApplicationController
 
   def update
     if params[:id] and params[:delete_item]
-      CartMethods.remove_item_from_list(cookies,params[:id])
+      render :text=> CartMethods.remove_item_from_list(cookies,params[:id]).to_s
     elsif params[:id]
-      CartMethods.add_item_to_list(cookies,params[:id],{:title=>params[:title],:price=>params[:price].to_f, :count => 1})
+      render :text => CartMethods.add_item_to_list(cookies,params[:id],{:title=>params[:title],:price=>params[:price].to_f, :count => params[:inc].to_i}).to_s
     else
       redirect_to :home and return
     end
-    render :text=> "true"
   end
 
   def show
@@ -25,6 +24,7 @@ class ShopCartsController < ApplicationController
 
   def check_cookies_valid
     redirect_to root_path if !CartMethods.cookies_valid?(cookies)
+
   end
 
 end
