d5cc2400e0c7369a6d6e232b0c58f898f6a9f2aa
Max <ax@mail.ru>
2011-09-23 00:23:53 +0300

    Edited app/models/company.rb via GitHub

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,6 +1,6 @@
 class Company < ActiveRecord::Base
   has_many :products, :dependent => :destroy
-  has_many :groups, :through => :products
+  has_many :groups, :through => :products, :uniq => true
   has_many :photos, :as => :entity, :dependent => :destroy
 
   validates :name, :presence => true, :uniqueness => true, :length => {:minimum => 3, :maximum => 50}
