6ea9e65bb8d1128676f60944a04dc08e250c323f
Max <ax@mail.ru>
2011-09-23 00:21:30 +0300

    Edited app/models/group.rb via GitHub

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,6 +1,6 @@
 class Group < ActiveRecord::Base
   has_many :products, :dependent => :destroy
-  has_many :companies, :through => :products
+  has_many :companies, :through => :products, :uniq => true
   has_many :photos, :as => :entity, :dependent => :destroy
 
   validates :name, :presence => true, :uniqueness => true, :length => {:minimum => 4, :maximum => 50}
